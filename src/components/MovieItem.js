c7b892390c15abc50b81ce5fdb98075d83357d9e
孙昊翔 <673686754@qq.com>
2018-04-30 00:16:07 +0800

    no message

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react'
 import {
-	View,
-	Text,
-	Image
+  View,
+  Text,
+  Image
 } from 'react-native'
 import styles from '../styles/Main'
 
 const MovieItem = (props) => {
-	const {
-		itemData: {
-			images: { large },
-			title,
-			original_title,
-			year,
-			rating: { average }
-		}
-	} = props
+  const {
+    itemData: {
+      images: { large },
+      title,
+      original_title,
+      year,
+      rating: { average }
+    }
+  } = props
 
 	return (
-		<View style={styles.item}>
-			<Image
-				source={{uri: large}}
-				style={styles.itemImage}
-			/>
-			<View style={styles.itemContent}>
-				<Text style={styles.itemHeader}>{title}</Text>
-				<Text style={styles.itemMeta}>
-					{`${original_title} (${year})`}
-				</Text>
-				<Text style={styles.redText}>{average}</Text>
-			</View>
-		</View>
-	)
+    <View style={styles.item}>
+      <Image
+        source={{uri: large}}
+        style={styles.itemImage}
+      />
+      <View style={styles.itemContent}>
+      <Text style={styles.itemHeader}>{title}</Text>
+        <Text style={styles.itemMeta}>
+          {`${original_title} (${year})`}
+        </Text>
+        <Text style={styles.redText}>{average}</Text>
+      </View>
+    </View>
+  )
 }
 
 export default MovieItem
