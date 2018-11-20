4eb9015f8bcbb525595f49b28d280426afa13dc3
孙昊翔 <673686754@qq.com>
2018-11-20 12:10:54 +0800

    适配iPhone XS Max

--- src/utils/isIphoneX.js
+++ src/utils/isIphoneX.js
@@ -4,19 +4,24 @@ import {
 } from 'react-native'
 
 const isIphoneX = () => {
-  // iPhoneX的宽和高
+  if (Platform.OS !== 'ios') {
+    return false
+  }
+  // iPhoneX / iPhoneXs Max 的宽和高
   const xWidth = 375
   const xHeight = 812
+  const xsMaxWidth = 414
+  const xsMaxHeight = 896
 
   // 设备的宽和高
   const screenWidth = Dimensions.get('window').width
   const screenHeight = Dimensions.get('window').height
 
+  const isIphoneX = ((screenWidth === xWidth && screenHeight === xHeight) || (screenWidth === xHeight && screenHeight === xWidth))
+  const isIphoneXsMax = ((screenWidth === xsMaxWidth && screenHeight === xsMaxHeight) || (screenWidth === xsMaxHeight && screenHeight === xsMaxWidth))
+
   // 通过设备宽高与iPhoneX宽高是否相同来判断（需考虑横屏）
-  return (
-    Platform.OS === 'ios' &&
-    ((screenWidth === xWidth && screenHeight === xHeight) || (screenWidth === xHeight && screenHeight === xWidth))
-  )
+  return (isIphoneX || isIphoneXsMax)
 }
 
 export default isIphoneX
